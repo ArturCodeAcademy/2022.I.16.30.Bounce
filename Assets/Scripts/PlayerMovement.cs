c3856d32c0d6774205fd50f36a040f4dca9370e5
ArturCodeAcademy <artur.radzivilov@codeacademylt.onmicrosoft.com>
2023-05-15 17:57:09 +0300

    2_15 pamoka

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,34 @@
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _horizontalForce;
-    [SerializeField] private float _jumpImpulse;
-    
+    [SerializeField, Min(0.1f)] private float _horizontalForce;
+    [SerializeField, Min(0.1f)] private float _jumpImpulse;
+    [SerializeField, Min(1)] private int _jumpCount;
+
+    private int _leftJumps;
     private Transform _camera;
     private Rigidbody _rigidbody;
 
     private void Start()
     {
+        _leftJumps = _jumpCount;
         _camera = Camera.main.transform;
         _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (_leftJumps > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 velocity = _rigidbody.velocity;
+            velocity.y = 0;
+            _rigidbody.velocity = velocity;
+            _rigidbody.AddForce(Vector3.up * _jumpImpulse, ForceMode.Impulse);
+            _leftJumps--;
+        }
     }
 
     private void FixedUpdate()
@@ -26,9 +42,12 @@ public class PlayerMovement : MonoBehaviour
         directionForward.y = directionRight.y = 0;
         Vector3 force = input.x * directionRight + input.z * directionForward;
         force *= _horizontalForce;
-        _rigidbody.AddForce(force);
+        _rigidbody.AddForce(force);      
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space)) // Fix
-            _rigidbody.AddForce(Vector3.up * _jumpImpulse, ForceMode.Impulse);
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.contacts.Any(x => x.point.y < transform.position.y))
+            _leftJumps = _jumpCount;
     }
 }
